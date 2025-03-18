177bd8918d25e8687ec0c13489f1c1ed19acc61c
ivan <iegc71@gmail.com>
2025-03-18 13:32:05 -0500

    primero crear los custom post y luego hace flush_rewrite_rules

--- prompts-plugin.php
+++ prompts-plugin.php
@@ -25,21 +25,39 @@ require_once PROMPTS_PLUGIN_DIR . 'includes/class-templates.php';
 require_once PROMPTS_PLUGIN_DIR . 'includes/class-settings.php';
 require_once PROMPTS_PLUGIN_DIR . 'includes/helpers.php';
 
-// Inicializar las clases
-add_action('plugins_loaded', function() {
-    new Prompts_Post_Type();
+// Función para inicializar la clase Prompts_Post_Type y registrar el post type
+function prompts_register_post_type() {
+    $post_type = new Prompts_Post_Type();
+    $post_type->register_post_type(); // Llamamos directamente al método de registro
+}
+
+// Función para inicializar las clases adicionales
+function initialize_prompts_additional_classes() {
     new Prompts_Taxonomy();
     new Prompts_Metaboxes();
     new Prompts_Scripts();
     new Prompts_Templates();
     new Prompts_Settings();
-});
-function prompts_flush_rewrite_rules() {
-    flush_rewrite_rules();
 }
-register_activation_hook(__FILE__, 'prompts_flush_rewrite_rules');
 
-function prompts_deactivate() {
-    flush_rewrite_rules();
+// Registro normal en init
+add_action('init', function() {
+    prompts_register_post_type();
+    initialize_prompts_additional_classes();
+});
+
+// Función de activación
+function prompts_activate_function() {
+    // Registrar el post type inmediatamente
+    prompts_register_post_type();
+
+    // Forzar actualización de reglas de reescritura
+    flush_rewrite_rules(true);
 }
-register_deactivation_hook(__FILE__, 'prompts_deactivate');
+register_activation_hook(__FILE__, 'prompts_activate_function');
+
+// Función de desactivación
+function prompts_deactivate_function() {
+    flush_rewrite_rules(true);
+}
+register_deactivation_hook(__FILE__, 'prompts_deactivate_function');
